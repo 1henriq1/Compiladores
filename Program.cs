fe468a4b355d310eae652fc3233bf52ec0174f5d
Henrique <schmitthenriq@gmail.com>
2017-10-01 23:19:50 -0300

    Adicionado arquivos da Etapa 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String c = "";
+            c = Console.ReadLine();
+            AntlrInputStream inputStream = new AntlrInputStream(c);
+
+            GrammLexer lexer = new GrammLexer(inputStream);
+
+
+            CommonTokenStream bts = new CommonTokenStream(lexer);
+            bts.Fill();
+            IToken tk = lexer.NextToken();
+
+            GrammParser p = new GrammParser(bts);
+            p.start();
+            while (1 == 1)
+            {
+
+            }
+        }
+    }
+}
